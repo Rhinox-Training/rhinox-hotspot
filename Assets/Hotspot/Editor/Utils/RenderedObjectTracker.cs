9786975b3d6594d02dfb80d6dafcb014e0463732
Gaetan Deglorie <gaetan@rhinox.training>
2023-05-03 10:36:57 +0200

    [ADD] RenderedObjectTracker

---
+++ Assets/Hotspot/Editor/Utils/RenderedObjectTracker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace Hotspot.Editor
+{
+    public class RenderedObjectTracker : IDisposable
+    {
+        private Renderer[] _allRenderersInScene;
+        private bool _initialized = false;
+
+
+        public void Initialize()
+        {
+            if (_initialized)
+                return;
+            
+            _initialized = true;
+        }
+
+        public void Terminate()
+        {
+            if (!_initialized)
+                return;
+
+            _initialized = false;
+        }
+        
+
+        private void RefreshMeshRendererCache()
+        {
+            _allRenderersInScene = GameObject.FindObjectsOfType<Renderer>();
+        }
+
+        public void Dispose()
+        {
+            Terminate();
+        }
+    }
+}
