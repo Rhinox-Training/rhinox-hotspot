25b357b94505e2f25433c65f977eceef1ceac2a7
Gaetan Deglorie <gaetan@rhinox.training>
2023-05-03 14:50:44 +0200

    [MOD] Base files for statistics

---
+++ Editor/Benchmarks/Statistic/ViewedObjectBenchmarkStatistic.cs
@@ -0,0 +1,10 @@
+﻿namespace Hotspot.Editor
+{
+    public abstract class ViewedObjectBenchmarkStatistic : BaseBenchmarkStatistic
+    {
+        public override void Sample()
+        {
+            
+        }
+    }
+}
