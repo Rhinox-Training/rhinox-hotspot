a59a445bdf9ad22e51ac425840157fec4f73cdee
Gaetan Deglorie <gaetan@rhinox.training>
2023-05-03 10:54:06 +0200

    [MOD] RenderedObjectTracker implementation

--- Assets/Hotspot/Editor/Utils/RenderedObjectTracker.cs
+++ Assets/Hotspot/Editor/Utils/RenderedObjectTracker.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using Rhinox.Lightspeed;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Hotspot.Editor
 {
@@ -7,12 +11,18 @@ namespace Hotspot.Editor
     {
         private Renderer[] _allRenderersInScene;
         private bool _initialized = false;
+        private Renderer[] _filteredRenderers;
 
+        public delegate void VisibleRenderersChangedHandler(RenderedObjectTracker sender, ICollection<Renderer> visibleRenderers);
+        public event VisibleRenderersChangedHandler VisibleRenderersChanged;
 
         public void Initialize()
         {
             if (_initialized)
                 return;
+            
+            RefreshMeshRendererCache();
+            EditorSceneManager.sceneOpened += OnSceneOpened;
             
             _initialized = true;
         }
@@ -22,18 +32,44 @@ namespace Hotspot.Editor
             if (!_initialized)
                 return;
 
+            EditorSceneManager.sceneOpened -= OnSceneOpened;
             _initialized = false;
-        }
-        
-
-        private void RefreshMeshRendererCache()
-        {
-            _allRenderersInScene = GameObject.FindObjectsOfType<Renderer>();
         }
 
         public void Dispose()
         {
             Terminate();
+        }
+
+        public void UpdateForCamera(Camera camera)
+        {
+            if (camera == null)
+                return;
+
+            var filteredRenderers = new List<Renderer>();
+            foreach (var r in _allRenderersInScene)
+            {
+                if (!r.isVisible)
+                    continue;
+
+                if (!r.IsWithinFrustum(camera))
+                    continue;
+
+                filteredRenderers.Add(r);
+            }
+            _filteredRenderers = filteredRenderers.ToArray();
+
+            VisibleRenderersChanged?.Invoke(this, _filteredRenderers);
+        }
+        
+        private void RefreshMeshRendererCache()
+        {
+            _allRenderersInScene = GameObject.FindObjectsOfType<Renderer>();
+        }
+
+        private void OnSceneOpened(Scene scene, OpenSceneMode mode)
+        {
+            RefreshMeshRendererCache();
         }
     }
 }
