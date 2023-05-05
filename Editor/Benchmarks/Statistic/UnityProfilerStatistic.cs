24b5edf4a0516999bf984ba439a944e2c96b6387
Gaetan Deglorie <gaetan@rhinox.training>
2023-05-05 16:47:11 +0200

    [ADD] Additional stats

---
+++ Editor/Benchmarks/Statistic/UnityProfilerStatistic.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Rhinox.Lightspeed.Reflection;
+using Sirenix.OdinInspector;
+using Unity.Profiling;
+using UnityEngine;
+
+namespace Hotspot.Editor
+{
+    public static class UnityDebugProfilerStats
+    {
+        public static ProfilerStat UsedSystemMemory = new ProfilerStat(ProfilerCategory.Memory, "System Used Memory");
+        public static ProfilerStat GCReservedMemory = new ProfilerStat(ProfilerCategory.Memory, "GC Reserved Memory");
+        
+        public static ProfilerStat[] All
+        {
+            get
+            {
+                return new[]
+                {
+                    UsedSystemMemory,
+                    GCReservedMemory
+                };
+            }
+        }
+    }
+
+    [Serializable]
+    public class ProfilerStat : ISerializationCallbackReceiver
+    {
+        [NonSerialized]
+        public ProfilerCategory Category;
+        public string StatName;
+
+        [SerializeField, HideInInspector]
+        private ushort _serializedCategory;
+
+        private FieldInfo _categoryField;
+
+        public ProfilerStat(ProfilerCategory category, string name)
+        {
+            Category = category;
+            StatName = name;
+        }
+
+
+        public void OnBeforeSerialize()
+        {
+            if (_categoryField == null)
+                _categoryField = typeof(ProfilerCategory).GetField("m_CategoryId",BindingFlags.Instance | BindingFlags.NonPublic);
+
+            _serializedCategory = (ushort)_categoryField.GetValue(Category);
+        }
+
+        public void OnAfterDeserialize()
+        {
+            if (_categoryField == null)
+                _categoryField = typeof(ProfilerCategory).GetField("m_CategoryId",BindingFlags.Instance | BindingFlags.NonPublic);
+            var properties = typeof(ProfilerCategory).GetProperties(BindingFlags.Public | BindingFlags.Static);
+            Category = properties.Where(x =>
+                    x.GetReturnType() == typeof(ProfilerCategory) &&
+                    _serializedCategory == (ushort) _categoryField.GetValue(x.GetValue(null)))
+                .Select(x => (ProfilerCategory)x.GetValue(null))
+                .FirstOrDefault();
+        }
+    }
+    
+    
+    public class UnityProfilerStatistic : BaseMeasurableBenchmarkStatistic
+    {
+        private ProfilerRecorder _recorder;
+
+        [ValueDropdown(nameof(GetOptions))]
+        public ProfilerStat Stat;
+
+        private bool _disposed;
+        private float _sampleCache;
+
+        public override void StartNewRun()
+        {
+            base.StartNewRun();
+            _disposed = false;
+            _recorder = ProfilerRecorder.StartNew(Stat.Category, Stat.StatName);
+        }
+
+        protected override string GetStatName()
+        {
+            return Stat.StatName;
+        }
+
+        public override void CleanUp()
+        {
+            _recorder.Dispose();
+            _disposed = true;
+            base.CleanUp();
+        }
+
+        protected override float SampleStatistic()
+        {
+            if (_disposed)
+                return _sampleCache;
+
+            _sampleCache = (float)_recorder.LastValue;
+            return _sampleCache;
+        }
+
+        protected ValueDropdownItem[] GetOptions()
+        {
+            return UnityDebugProfilerStats.All.Select(x => new ValueDropdownItem(x.StatName, x)).ToArray();
+        }
+    }
+}
