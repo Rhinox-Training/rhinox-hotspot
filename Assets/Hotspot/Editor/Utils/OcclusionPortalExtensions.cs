9d3171181bd9ef282030214d1f047075067bde5f
Gaetan Deglorie <gaetan@rhinox.training>
2023-05-09 17:01:51 +0200

    [FIX] Null ref on MaterialRenderingAnalysis
    [ADD] OcclusionPortalExtension to set and read bounds data of OcclusionPortal

---
+++ Assets/Hotspot/Editor/Utils/OcclusionPortalExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Rhinox.GUIUtils.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Hotspot.Editor
+{
+    public static class OcclusionPortalExtensions
+    {
+        private const string k_CenterPath = "m_Center";
+        private const string k_SizePath = "m_Size";
+
+        public static void UpdateBounds(this OcclusionPortal portal, Vector3 center, Vector3 size)
+        {
+            var so = new SerializedObject(portal);
+            var centerProperty = so.FindProperty(k_CenterPath);
+            centerProperty.SetValue(center);
+            var sizeProperty = so.FindProperty(k_SizePath);
+            sizeProperty.SetValue(size);
+        }
+
+        public static void UpdateBounds(this OcclusionPortal portal, Bounds bounds)
+        {
+            UpdateBounds(portal, bounds.center, bounds.size);
+        }
+
+        public static Bounds GetBounds(this OcclusionPortal portal)
+        {
+            var so = new SerializedObject(portal);
+            var centerProperty = so.FindProperty(k_CenterPath);
+            Vector3 center = (Vector3) centerProperty.GetValue();
+            var sizeProperty = so.FindProperty(k_SizePath);
+            Vector3 size = (Vector3) sizeProperty.GetValue();
+            return new Bounds(center, size);
+        }
+    }
+}
