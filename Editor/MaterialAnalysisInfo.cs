7902e23a10bff40324ba3092ed336d89ea732ead
Tristan Wauthier <tristan.wauthier@hotmail.com>
2023-05-10 10:55:51 +0200

    [ADD] Material keyword usage is now fetched

--- Editor/MaterialAnalysisInfo.cs
+++ Editor/MaterialAnalysisInfo.cs
@@ -4,21 +4,69 @@ using Rhinox.GUIUtils.Editor;
 using Rhinox.Lightspeed;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace Hotspot.Editor
 {
     public class MaterialAnalysisInfo : AnalysisInfo
     {
-        private Material _material;
         private int _amountOfOccurrences;
+        public int AmountOfOccurrences => _amountOfOccurrences;
+
+        private Material _material;
         private bool _isUnfolded;
         private IEnumerable<KeyValuePair<Mesh, GameObject>> _materialUses;
+        private IEnumerable<KeyValuePair<string, int>> _keyWordCombinations;
 
-        public MaterialAnalysisInfo(Material material, int amountOfOccurrences, IEnumerable<Renderer> renderers)
+        public MaterialAnalysisInfo(Material material, int amountOfOccurrences, IEnumerable<Renderer> renderers,
+            IEnumerable<Material> allMaterials)
         {
             _material = material;
             _amountOfOccurrences = amountOfOccurrences;
             _materialUses = GetMaterialUses(renderers);
+            GetKeywordSpread(allMaterials);
+        }
+
+        private void GetKeywordSpread(IEnumerable<Material> allMaterials)
+        {
+            // Get all materials that match this info's material
+            var materials = allMaterials.Where(x => x == _material);
+
+            // Get all combinations of keywords
+            var keyWordCombinations = materials.Select(x => x.enabledKeywords);
+
+            // Group them according to their distinct set and count them
+            var groups = keyWordCombinations.GroupBy(x => new HashSet<LocalKeyword>(x))
+                .Select(g => new KeyValuePair<IEnumerable<LocalKeyword>, int>(g.Key, g.Count()));
+
+            // Create the return value
+            Dictionary<string, int> returnValue = new ();
+            
+            // Process all the groups
+            foreach (var group in groups)
+            {
+                string keywordCombo = group.Key.Aggregate(string.Empty, (current, keyWords) => current + (keyWords.ToString() + " "));
+                if (returnValue.ContainsKey(keywordCombo))
+                    returnValue[keywordCombo] += group.Value;
+                else
+                    returnValue.Add(keywordCombo, group.Value);
+            }
+
+            // Replace the entry with an empty string as key
+            foreach (var pair in returnValue)
+            {
+                if (pair.Key == string.Empty)
+                {
+                    int pairValue = returnValue[pair.Key];
+                    string newKey = "NO_KEYWORDS";
+                    returnValue.Add(newKey, pairValue);
+                    returnValue.Remove(pair.Key);
+                    break;
+                }
+            }
+            
+            // Set the value
+            _keyWordCombinations = returnValue;
         }
 
         private IEnumerable<KeyValuePair<Mesh, GameObject>> GetMaterialUses(IEnumerable<Renderer> renderers)
@@ -56,7 +104,16 @@ namespace Hotspot.Editor
                 GUILayout.Label(keywordName, EditorStyles.miniLabel);
             }
 
-            GUILayout.Space(5f);
+            GUILayout.Space(10f);
+            GUILayout.Label("Material Keyword Combinations", EditorStyles.boldLabel);
+            GUILayout.Label($"Amount of combinations: {_keyWordCombinations.Count()}", EditorStyles.boldLabel);
+            foreach (var pair in _keyWordCombinations)
+            {
+                GUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField(pair.Key.ToString());
+                EditorGUILayout.LabelField(pair.Value.ToString());
+                GUILayout.EndHorizontal();
+            }
 
             GUILayout.BeginHorizontal();
             GUILayout.Label("Applied Mesh", EditorStyles.boldLabel);
@@ -73,7 +130,7 @@ namespace Hotspot.Editor
                 {
                     EditorGUIUtility.PingObject(materialUsePair.Value);
                 }
-            
+
                 GUILayout.EndHorizontal();
             }
         }
