65e6f69279a7a9242bac5f38c765e0fdb8d030e7
Glenn Quintyn <glenn.quintyn@gmail.com>
2023-05-11 14:51:14 +0200

    cleanup and QOL to occlusion wizards.
    [MOD] now using constants to draw button height.
    [ADD] Remove all in scene buttons.
    [MOD] moved some of the ONGUI code into scopes for readability.

--- Editor/OcclusionAreaWindow.cs
+++ Editor/OcclusionAreaWindow.cs
@@ -15,12 +15,15 @@ using UnityEngine.UIElements;
 public class OcclusionAreaWindow : CustomEditorWindow
 {
     private const string _rootObjName = "__GeneratedOcllusionAreas__";
+    private const float _scrollViewMaxHeight = 200f;
+    private const float _normalButtonHeight = 30f;
+    private const float _RemoveAllButtonHeight = 20f;
     private const int _navMeshAreaCount = 32;
-    private const float _scrollViewMaxHeight = 150f;
+
     private GameObject _rootObj = null;
 
     [NavMeshArea(true)]
-    public int _navMeshAreas = -1;
+    public int _navMeshAreasMask = -1;
 
     private SerializedObject _serObj = null;
     private SerializedProperty _serProp = null;
@@ -28,16 +31,12 @@ public class OcclusionAreaWindow : CustomEditorWindow
     private float _height = 2.5f;
     private float _margin = .5f;
 
-    //private object _layerMask;
-
-    Dictionary<GameObject, OcclusionArea> _occlusionAreas = new Dictionary<GameObject, OcclusionArea>();
+    //Dictionary<GameObject, OcclusionArea> _occlusionAreas = new Dictionary<GameObject, OcclusionArea>();
+    HashSet<OcclusionArea> _occlusionAreas = new HashSet<OcclusionArea>();
     private Vector2 _scrollPos;
     private int _selectedAreaIndex;
 
-    //
-
     [MenuItem("Tools/HotSpot/Occlusion Area Editor", false, 50)]
-
     public static void ShowWindow()
     {
         var win = GetWindow(typeof(OcclusionAreaWindow));
@@ -49,7 +48,7 @@ public class OcclusionAreaWindow : CustomEditorWindow
         base.Initialize();
 
         _serObj = new SerializedObject(this);
-        _serProp = _serObj.FindProperty("_navMeshAreas");
+        _serProp = _serObj.FindProperty("_navMeshAreasMask");
     }
 
     private void GenerateOcclusionAreas()
@@ -59,9 +58,10 @@ public class OcclusionAreaWindow : CustomEditorWindow
         if (_rootObj == null)
             _rootObj = new GameObject(_rootObjName);
 
+        int cnt = 0;
         for (int idx = 0; idx < _navMeshAreaCount; idx++)
         {
-            if (((_navMeshAreas >> idx) & 1) == 0)
+            if (((_navMeshAreasMask >> idx) & 1) == 0)
                 continue;
 
             var nave = NavMeshHelper.CalculateTriangulation(1 << idx);
@@ -75,53 +75,40 @@ public class OcclusionAreaWindow : CustomEditorWindow
                 recto = recto.Encapsulate(new Vector2(nave.vertices[verteIdx].x, nave.vertices[verteIdx].z));
             }
 
-            GameObject obj = GameObject.Find($"OcclusionArea{idx}");
-            obj = obj != null ? obj : new GameObject($"OcclusionArea{idx}");
-            //if (_occlusionAreas.TryGetValue())
-            //{
-
-            //}
-
-            var occlusionAreaCmpt = obj.GetOrAddComponent<OcclusionArea>();
-            obj.transform.parent = _rootObj.transform;
-
-            occlusionAreaCmpt.center = new Vector3(recto.x + (recto.width) / 2, nave.vertices[0].y + (_height / 2), recto.y + (recto.height) / 2);
-            occlusionAreaCmpt.size = new Vector3(recto.width + (_margin * 2), _height, recto.height + (_margin * 2));
-
-            _occlusionAreas.TryAdd(obj, occlusionAreaCmpt);
+            string autoGenName = $"_Auto_OcclusionArea_{cnt}";
+            CreateOrAddNewOcclusionArea(nave, recto, autoGenName);
+            ++cnt;
         }
+    }
+
+    private void CreateOrAddNewOcclusionArea(NavMeshTriangulation nave, Rect recto, string autoGenName)
+    {
+        GameObject obj = GameObject.Find(autoGenName);
+        obj = obj != null ? obj : new GameObject(autoGenName);
+
+        var occlusionAreaCmpt = obj.GetOrAddComponent<OcclusionArea>();
+        obj.transform.parent = _rootObj.transform;
+
+        occlusionAreaCmpt.center = new Vector3(recto.x + (recto.width) / 2, nave.vertices[0].y + (_height / 2), recto.y + (recto.height) / 2);
+        occlusionAreaCmpt.size = new Vector3(recto.width + (_margin * 2), _height, recto.height + (_margin * 2));
+
+        _occlusionAreas.Add(occlusionAreaCmpt);
     }
 
     private void GetAllOcclusionAreas()
     {
         _rootObj = _rootObj != null ? _rootObj : GameObject.Find(_rootObjName);
 
-        if (_rootObj != null)
+        var occlusionAreas = UnityEngine.Object.FindObjectsOfType<OcclusionArea>();
+
+        foreach (var area in occlusionAreas)
         {
-            int cnt = _rootObj.transform.childCount;
-
-            for (int idx = 0; idx < cnt; ++idx)
-            {
-                var child = _rootObj.transform.GetChild(idx);
-                if (child.TryGetComponent<OcclusionArea>(out var cmpnt))
-                    _occlusionAreas.Add(child.gameObject, cmpnt);
-            }
+            _occlusionAreas.Add(area);
         }
-
-        //var OcclusionAreas = UnityEngine.Object.FindObjectsOfType<MeshRenderer>();
-    }
-
-    private void SelectItemInScene(KeyValuePair<GameObject, OcclusionArea> item)
-    {
-        Selection.activeObject = item.Value;
-        EditorGUIUtility.PingObject(item.Key);
-        SceneView.lastActiveSceneView.Frame(new Bounds(item.Value.center, item.Value.size), false);
     }
 
     protected override void OnGUI()
     {
-        float _width = GUI.skin.label.CalcSize(new GUIContent("#9999:")).x;
-
         EditorGUILayout.Space(5f);
         EditorGUILayout.PropertyField(_serProp);
         if (_serObj.hasModifiedProperties)
@@ -133,28 +120,69 @@ public class OcclusionAreaWindow : CustomEditorWindow
         _margin = EditorGUILayout.FloatField("Area margin", _margin, GUILayout.ExpandWidth(true));
 
         EditorGUILayout.Space(5f);
-        EditorGUILayout.BeginHorizontal();
-        if (GUILayout.Button("Generate New Areas", GUILayout.ExpandWidth(true)))
-            GenerateOcclusionAreas();
-        if (GUILayout.Button("Get All Areas Scene ", GUILayout.ExpandWidth(true)))
-            GetAllOcclusionAreas();
-        EditorGUILayout.EndHorizontal();
+
+        {
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("Generate New Areas", GUILayout.Height(_normalButtonHeight), GUILayout.ExpandWidth(true)))
+                GenerateOcclusionAreas();
+            if (GUILayout.Button("Get All Areas Scene", GUILayout.Height(_normalButtonHeight), GUILayout.ExpandWidth(true)))
+                GetAllOcclusionAreas();
+            EditorGUILayout.EndHorizontal();
+        }
+
+        EditorGUILayout.Space(5f);
+        if (GUILayout.Button("REMOVE ALL OCCLUSION AREAS IN SCENE", GUILayout.Height(_RemoveAllButtonHeight), GUILayout.ExpandWidth(true)))
+            RemoveAllAreas();
+        EditorGUILayout.Space(5f);
+
+        { 
+        _scrollPos = EditorGUILayout.BeginScrollView(_scrollPos, GUILayout.ExpandHeight(true), GUILayout.MaxHeight(_scrollViewMaxHeight));
+        ShowExistingAreas();
+        EditorGUILayout.EndScrollView();
+        }
 
         EditorGUILayout.Space(5f);
 
-        _scrollPos = EditorGUILayout.BeginScrollView(_scrollPos, GUILayout.ExpandHeight(true), GUILayout.MaxHeight(_scrollViewMaxHeight));
+        {
+            EditorGUILayout.BeginHorizontal();
+            ProcessFooterButtons();
+            EditorGUILayout.EndHorizontal();
+        }
+
+        EditorGUILayout.EndVertical();
+    }
+
+    private void SelectItemInScene(OcclusionArea area)
+    {
+        Selection.activeObject = area.gameObject;
+        EditorGUIUtility.PingObject(area);
+        SceneView.lastActiveSceneView.Frame(new Bounds(area.center + area.gameObject.transform.position, Vector3.one), false);
+    }
+
+    private void RemoveAllAreas()
+    {
+        _occlusionAreas.Clear();
+        var areas = UnityEngine.Object.FindObjectsOfType<OcclusionArea>();
+
+        foreach (var area in areas)
+            DestroyImmediate(area);
+    }
+
+    private void ShowExistingAreas()
+    {
+        float _width = GUI.skin.label.CalcSize(new GUIContent("#9999:")).x;
 
         int index = 0;
-        foreach (var item in _occlusionAreas)
+        foreach (var area in _occlusionAreas)
         {
             EditorGUILayout.BeginHorizontal();
             GUILayout.Label($"#{index}", GUILayout.Width(_width));
 
             if (index != _selectedAreaIndex)
             {
-                if (GUILayout.Button($"{item.Key.name}", GUILayout.ExpandWidth(true)))
+                if (GUILayout.Button($"{area.gameObject.name}", GUILayout.ExpandWidth(true)))
                 {
-                    SelectItemInScene(item);
+                    SelectItemInScene(area);
                     _selectedAreaIndex = index;
                 }
             }
@@ -162,25 +190,24 @@ public class OcclusionAreaWindow : CustomEditorWindow
             {
                 using (new eUtility.GuiBackgroundColor(Color.gray))
                 {
-                    if (GUILayout.Button($"{item.Key.name}", GUILayout.ExpandWidth(true)))
-                        SelectItemInScene(item);
+                    if (GUILayout.Button($"{area.gameObject.name}", GUILayout.ExpandWidth(true)))
+                        SelectItemInScene(area);
                 }
             }
 
             EditorGUILayout.EndHorizontal();
             ++index;
         }
+    }
 
-        EditorGUILayout.EndScrollView();
-
-        EditorGUILayout.Space(5f);
-        EditorGUILayout.BeginHorizontal();
-
+    private void ProcessFooterButtons()
+    {
         using (new eUtility.DisabledGroup(_occlusionAreas.Count == 0))
         {
             if (GUILayout.Button("<-- Previous", GUILayout.ExpandWidth(true)))
             {
                 --_selectedAreaIndex;
+
                 if (_selectedAreaIndex <= 0)
                     _selectedAreaIndex = 0;
 
@@ -191,9 +218,9 @@ public class OcclusionAreaWindow : CustomEditorWindow
             {
                 if (_selectedAreaIndex >= 0 && _selectedAreaIndex < _occlusionAreas.Count)
                 {
-                    var pair = _occlusionAreas.ElementAt(_selectedAreaIndex);
-                    DestroyImmediate(pair.Value);
-                    _occlusionAreas.Remove(pair.Key);
+                    var area = _occlusionAreas.ElementAt(_selectedAreaIndex);
+                    _occlusionAreas.Remove(area);
+                    DestroyImmediate(area);
 
                     if (_selectedAreaIndex >= _occlusionAreas.Count)
                         _selectedAreaIndex = _occlusionAreas.Count - 1;
@@ -205,17 +232,12 @@ public class OcclusionAreaWindow : CustomEditorWindow
             if (GUILayout.Button("Next -->", GUILayout.ExpandWidth(true)))
             {
                 ++_selectedAreaIndex;
+
                 if (_selectedAreaIndex >= _occlusionAreas.Count)
                     _selectedAreaIndex = _occlusionAreas.Count - 1;
 
                 SelectItemInScene(_occlusionAreas.ElementAt(_selectedAreaIndex));
             }
         }
-
-        EditorGUILayout.EndHorizontal();
-
-        //_navMeshAreas = EditorGUILayout.MaskField(_navMeshAreas, _layerMaskOptions.ToArray());
-
-        EditorGUILayout.EndVertical();
     }
 }
