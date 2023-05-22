47c33e10d52d71a29ef5444adb1a574874aca147
Tristan Wauthier <tristan.wauthier@hotmail.com>
2023-05-22 15:43:48 +0200

    [ADD] Added an overlay tool window for the LODGroup

--- Scripts/Utils/LODGroupExtensions.cs
+++ Scripts/Utils/LODGroupExtensions.cs
@@ -25,32 +25,18 @@ public static class LODGroupExtensions
     }
 
     ///<summary>
-    ///Calculates the current height percentage of the LODGroup relative to the Camera.
+    ///Calculates the current transition percentage of the LODGroup relative to the Camera.
     ///</summary>
     ///<param name = "lodGroup" > The LODGroup whose height percentage is being calculated.</param>
     ///<param name = "cam" > The Camera to which the LODGroup's height percentage is being calculated relative to.</param>
-    ///<returns>The current height percentage of the LODGroup relative to the Camera.</returns>
+    ///<returns>The current transition percentage of the LODGroup relative to the Camera.</returns>
     ///<exception cref = "System.ArgumentNullException">Thrown if the distance between the LODGroup and Camera is zero.</exception>
     ///<example>
-    ///<code>float heightPercentage = lodGroup.CalculateCurrentHeightPercentage(mainCamera);</code>
+    ///<code>float transitionPercentage = lodGroup.CalculateCurrentTransitionPercentage(mainCamera);</code>
     ///</example>
-    public static float CalculateCurrentHeightPercentage(this LODGroup lodGroup, Camera cam)
+    public static float CalculateCurrentTransitionPercentage(this LODGroup lodGroup, Camera cam)
     {
-        // Calculates the distance between LODGroup and the camera
-        float distance = Vector3.Distance(lodGroup.transform.position, cam.transform.position);
-        if (distance == 0f)
-        {
-            PLog.Error<HotspotLogger>(
-                "Distance between LODGroup and Camera is zero, can't proceed as it will lead to division by zero.");
-            return -1;
-        }
-
-        // Computes the height based on the size of the LODGroup and the multiplier
-        float height = lodGroup.size;
-
-        // Calculates the relative height based on the reciprocal of the distance times the height
-        // Also check the lodBias in the QualitySettings
-        return 100f / distance * height * QualitySettings.lodBias;
+        return CalculateCurrentHeight(lodGroup, cam) * 100f;
     }
 
     public static float CalculateCurrentHeight(this LODGroup lodGroup, Camera cam)
