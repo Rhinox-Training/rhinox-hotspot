f403e7e7bea85bea1f279cd0a79068386e36eceb
Tristan Wauthier <tristan.wauthier@hotmail.com>
2023-06-07 13:07:54 +0200

    [MOD] Optimized vertex density heatmap calculation

--- Assets/Hotspot/Editor/URP/RenderingDebugger/SceneViewRenderSetup.cs
+++ Assets/Hotspot/Editor/URP/RenderingDebugger/SceneViewRenderSetup.cs
@@ -1,10 +1,8 @@
 ﻿using UnityEditor;
 using UnityEngine.Rendering;
 using System.Collections.Generic;
-using Rhinox.Lightspeed;
 using UnityEditor.SceneManagement;
 using UnityEngine;
-using UnityEngine.Profiling;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
 
@@ -20,7 +18,6 @@ namespace Hotspot.Editor
         private static VertexHeatmapRenderFeature.VertexHeatmapSettings _heatmapSettings = null;
 
         private static Camera _mainCamera;
-        private static Renderer[] _renderers;
 
         private static Color[] _pixels;
         private static Texture2D _densityTexture;
@@ -60,18 +57,24 @@ namespace Hotspot.Editor
                 _renderFeature.HeatmapSettings = _heatmapSettings;
             }
 
-            SceneView.beforeSceneGui -= CalculateVertexDensity;
-            SceneView.beforeSceneGui += CalculateVertexDensity;
+            EditorApplication.update -= SetRenderFeatureSettings;
+            EditorApplication.update += SetRenderFeatureSettings;
             EditorSceneManager.sceneOpened -= OnSceneOpened;
             EditorSceneManager.sceneOpened += OnSceneOpened;
 
             EditorApplication.quitting += OnQuit;
         }
 
+        private static void SetRenderFeatureSettings()
+        {
+            if(_renderFeature)
+                _renderFeature.HeatmapSettings = _heatmapSettings;
+        }
+
         private static void OnQuit()
         {
             EditorSceneManager.sceneOpened -= OnSceneOpened;
-            SceneView.beforeSceneGui -= CalculateVertexDensity;
+            EditorApplication.update -= SetRenderFeatureSettings;
 
             // Fetch the current render pipeline asset
             UniversalRenderPipelineAsset urpAsset = GraphicsSettings.currentRenderPipeline as
@@ -213,9 +216,6 @@ namespace Hotspot.Editor
 
         private static void OnSceneOpened(Scene scene, OpenSceneMode mode)
         {
-            // Refresh the render cache
-            _renderers = Object.FindObjectsOfType<Renderer>();
-
             // Fetch the current render pipeline asset
             UniversalRenderPipelineAsset urpAsset = GraphicsSettings.currentRenderPipeline as
                 UniversalRenderPipelineAsset;
@@ -224,6 +224,7 @@ namespace Hotspot.Editor
             urpAsset.RemoveAllRenderFeatures<VertexHeatmapRenderFeature>();
 
             _heatmapEnabled = false;
+            _renderFeature.ReleaseRenderers();
         }
 
         // Method to toggle the heatmap feature on and off
@@ -246,94 +247,13 @@ namespace Hotspot.Editor
             if (toggleValue)
             {
                 urpAsset.EnableRenderFeature<VertexHeatmapRenderFeature>();
-                CalculateVertexDensity(null);
+                _renderFeature!.HeatmapSettings = _heatmapSettings;
             }
             else
             {
                 urpAsset.DisableRenderFeature<VertexHeatmapRenderFeature>();
-                _renderers = null;
             }
         }
-
-        private static void CalculateVertexDensity(SceneView sceneView)
-        {
-            if (!_heatmapEnabled)
-                return;
-            _renderFeature.HeatmapSettings = _heatmapSettings;
-            Profiler.BeginSample("CalculateVertexDensity");
-
-            // Get Active camera
-            if (_mainCamera == null)
-                HotSpotUtils.TryGetMainCamera(out _mainCamera);
-
-            // Get all renderers
-            _renderers ??= Object.FindObjectsOfType<Renderer>();
-
-            int width = _mainCamera.pixelWidth;
-            int height = _mainCamera.pixelHeight;
-
-            //Init the texture and white pixels
-            if (_densityTexture == null || _densityTexture.width != width || _densityTexture.height != height)
-            {
-                _densityTexture = new Texture2D(width, height, TextureFormat.RGBAFloat, false)
-                {
-                    filterMode = FilterMode.Point,
-                    wrapMode = TextureWrapMode.Clamp
-                };
-
-                // Set the texture on the feature
-                _renderFeature.DensityTexture = _densityTexture;
-
-                // Create an array with the size of all the pixels in the texture.
-                _pixels = new Color[width * height];
-
-                _densityTexture.SetPixels(_pixels);
-            }
-
-            // Assure a gradient texture is set!
-            if (_heatmapSettings.HeatmapTexture == null)
-                _heatmapSettings.HeatmapTexture = GradientTexture;
-
-            // Reset all pixels to black and set all pixels in the texture
-            for (int i = 0; i < _pixels.Length; i++)
-                _pixels[i] = Color.black;
-
-            // Init the pixel step
-            float pixelStep = 1f / _heatmapSettings.MaxDensity;
-
-            // Loop over visible renderers
-            foreach (Renderer renderer in _renderers)
-            {
-                if (renderer != null && renderer.isVisible && renderer.IsWithinFrustum(_mainCamera))
-                {
-                    // Create MeshInfo
-                    var meshInfo = MeshInfo.Create(renderer);
-
-                    // Loop over vertices
-                    foreach (Vector3 meshVertex in meshInfo.Mesh.vertices)
-                    {
-                        // Transform the vertex to world space
-                        var worldSpaceVertex = renderer.transform.TransformPoint(meshVertex);
-
-                        //  Calculate screen space coordinates
-                        var screenPos = _mainCamera.WorldToScreenPoint(worldSpaceVertex);
-
-                        //  Filter out the ones outside view
-                        if (screenPos.x.IsBetween(0, width) && screenPos.y.IsBetween(0, height))
-                        {
-                            // Get the previous color   
-                            int index = (int)screenPos.y * width + (int)screenPos.x;
-                            _pixels[index].r += pixelStep;
-                        }
-                    }
-                }
-            }
-
-            // Set all pixels at once.
-            _densityTexture.SetPixels(_pixels);
-            _densityTexture.Apply();
-
-            Profiler.EndSample();
-        }
+        
     }
 }
